52d1edb16af871ba0c5be01fde9cf7889acc2dc4
Guilherme Silva <guissilva_@outlook.com>
2024-03-25 13:46:07 -0300

    FEATURE - exclusão de benchmark salvo.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import Comparar from '../src/services/comparar';
 import Estados from '../src/services/estados';
 import Salvar from '../src/services/salvar';
 import Listar from '../src/services/listar';
+import Excluir from './services/excluir';
 
 window.comparar = Comparar;
 window.salvar = Salvar;
 window.estados = Estados;
 window.listar = Listar;
+window.excluir = Excluir;
 
 Alpine.start();
---
+++ src/services/excluir.js
@@ -0,0 +1,10 @@
+import { api } from "./api.js";
+
+export default function Excluir() {
+  return {
+    async excluir(idBenchmark) {
+      idBenchmark = Number(idBenchmark);
+      await api.delete(`${api.defaults.baseURL}/deletar/${idBenchmark}`);
+    }
+  }
+}
